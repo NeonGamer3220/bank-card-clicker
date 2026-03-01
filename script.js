67d3431067fd759a4c9b125d70149750126b3d1b
NeonGamer3220 <neon.gamer322@gmail.com>
2026-03-01 15:53:34 +0100

    Added number formatting (K, M, B)

--- script.js
+++ script.js
@@ -77,6 +77,9 @@ const leaderboardList = document.getElementById('leaderboard-list');
 
 // Format numbers
 function formatMoney(num) {
+    if (num >= 1000000000) return (num / 1000000000).toFixed(1) + 'B';
+    if (num >= 1000000) return (num / 1000000).toFixed(1) + 'M';
+    if (num >= 1000) return (num / 1000).toFixed(1) + 'K';
     return Math.floor(num).toLocaleString();
 }
 
